029fc18bd1d724d7b099c4d376d6563a2470909a
vlaskinmac <tesseractmaks@gmail.com>
2022-03-18 15:40:55 +0300

    Setup filtering in admin UI.

--- property/admin.py
+++ property/admin.py
@@ -9,6 +9,7 @@ class PostAdmin(admin.ModelAdmin):
     readonly_fields = ['created_at']
     list_display = ['address', 'price', 'new_building', 'construction_year', 'town']
     list_editable = ['new_building']
+    list_filter = ['new_building', 'rooms_number', 'has_balcony']
 
 
 
