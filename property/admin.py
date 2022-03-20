0c98861def78f85ac4e8f10c9ab0fca5ab061f75
vlaskinmac <tesseractmaks@gmail.com>
2022-03-20 13:43:49 +0300

    Created a list of apartments and owners

--- property/admin.py
+++ property/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from .models import Flat, Claim, Owner
 
 
+class FlatsInline(admin.TabularInline):
+    model = Owner.apartments_in_property.through
+    raw_id_fields = ('flat', 'owner')
+
+
 @admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'owner')
@@ -11,6 +16,9 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ['new_building']
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
     raw_id_fields = ('author_like',)
+    inlines = [
+        FlatsInline,
+    ]
 
 
 @admin.register(Claim)
@@ -22,6 +30,9 @@ class ClaimAdmin(admin.ModelAdmin):
 class OwnerAdmin(admin.ModelAdmin):
     list_display = ['owner', 'phone_number', 'normalaized_phone_number']
     raw_id_fields = ('apartments_in_property',)
+    inlines = [
+        FlatsInline,
+    ]
 
 
 
